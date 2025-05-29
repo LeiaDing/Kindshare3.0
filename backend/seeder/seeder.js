c0a03330cdc60fa1c07a1560d30c24990c341149
Lei Ding <leiasxufe@gmail.com>
2025-05-29 12:02:06 -0400

    modified product.js to replace user required true with false

--- backend/seeder/seeder.js
+++ backend/seeder/seeder.js
@@ -4,12 +4,14 @@ import Product from "../models/product.js";
 import dotenv from "dotenv";
 
 dotenv.config({ path: "backend/config/config.env" });
+console.log("正在连接数据库：", process.env.DB_LOCAL_URI);
 
 const seedProducts = async () => {
   try {
-    await mongoose.connect-(process.env.DB_LOCAL_URI, {   // 🟥 改这一行：使用 .env 中的 DB_LOCAL_URI
+    await mongoose.connect(process.env.DB_LOCAL_URI, {   // 🟥 改这一行：使用 .env 中的 DB_LOCAL_URI
       useNewUrlParser: true,
       useUnifiedTopology: true,
+      serverSelectionTimeoutMS: 10000, // 🟥 加这个
     });
 
     await Product.deleteMany();
