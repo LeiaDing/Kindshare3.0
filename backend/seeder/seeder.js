57fedb44c8ed83bd62660d817b60a5909f01f815
Lei Ding <leiasxufe@gmail.com>
2025-05-29 10:56:13 -0400

    modified seeder with localUrl

--- backend/seeder/seeder.js
+++ backend/seeder/seeder.js
@@ -1,12 +1,16 @@
 import mongoose from "mongoose";
 import products from "./data.js";
 import Product from "../models/product.js";
+import dotenv from "dotenv";
+
+dotenv.config({ path: "backend/config/config.env" });
 
 const seedProducts = async () => {
   try {
-    await mongoose.connect(
-      "mongodb+srv://ghulamabbas:ghulamabbas@shopit.wkd8107.mongodb.net/shopit-v2?retryWrites=true&w=majority"
-    );
+    await mongoose.connect-(process.env.DB_LOCAL_URI, {   // 🟥 改这一行：使用 .env 中的 DB_LOCAL_URI
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
 
     await Product.deleteMany();
     console.log("Products are deleted");
