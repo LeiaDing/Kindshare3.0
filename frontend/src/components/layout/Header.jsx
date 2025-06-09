ce93dbc9be5c66a92ba658cc7b552313d3807c2b
Lei Ding <leiasxufe@gmail.com>
2025-06-09 13:13:04 -0400

    initial commit

--- frontend/src/components/layout/Header.jsx
+++ frontend/src/components/layout/Header.jsx
@@ -35,7 +35,7 @@ const Header = () => {
         <a href="/cart" style={{ textDecoration: "none" }}>
           <span id="cart" className="ms-3">
             {" "}
-            Cart{" "}
+            My Cart{" "}
           </span>
           <span className="ms-1" id="cart_count">
             {cartItems?.length}
